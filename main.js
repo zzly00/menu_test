edf98833cfd9386a9b8f84925b9abc3973629da4
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-09 16:49:02 +0800

    modified

--- main.js
+++ main.js
@@ -6,15 +6,14 @@ let main = document.querySelector('.main');
 let forScroll = document.querySelector('.forScroll');
 
 icon.addEventListener('click',function(){
-  console.log('aaa')
   // box.classList.toggle('show');
   header.classList.toggle('m_header');
 
-  // main.classList.toggle('main_fix');
+  main.classList.toggle('main_fix');
 
   forScroll.classList.toggle('open');
 
-  main.addEventListener('touchmove', function (e) {
-    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-  }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
+  // main.addEventListener('touchmove', function (e) {
+  //   e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  // }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
 });
