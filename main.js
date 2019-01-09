1f7f45c67ac88cc1b308f69e8e76bb698e7bafb8
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-09 16:43:00 +0800

    modified

--- main.js
+++ main.js
@@ -10,7 +10,11 @@ icon.addEventListener('click',function(){
   // box.classList.toggle('show');
   header.classList.toggle('m_header');
 
-  main.classList.toggle('main_fix');
+  // main.classList.toggle('main_fix');
 
   forScroll.classList.toggle('open');
+
+  main.addEventListener('touchmove', function (e) {
+    e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
+  }, {passive: false}); //passive 参数不能省略，用来兼容ios和android
 });
