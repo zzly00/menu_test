d92b41113f28321a9c9d4482fdb845784b695917
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-09 13:11:27 +0800

    test

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let box = document.querySelector('.box');
 let icon = document.querySelector('.icon');
 let header = document.querySelector('.header');
-let main = document.querySelector('.main');
+// let main = document.querySelector('.main');
 icon.addEventListener('click',function(){
   box.classList.toggle('show');
   header.classList.toggle('m_header');
 
-  main.classList.toggle('body_fix');
+  document.body.classList.toggle('body_fix');
 });
