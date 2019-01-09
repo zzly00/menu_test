2160eabc80020b427dd2506fa1ba067039dfce2a
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-09 11:33:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let box = document.querySelector('.box');
+let icon = document.querySelector('.icon');
+let header = document.querySelector('.header');
+icon.addEventListener('click',function(){
+  box.classList.toggle('show');
+  header.classList.toggle('m_header');
+  document.body.classList.toggle('body_fix');
+
+});
