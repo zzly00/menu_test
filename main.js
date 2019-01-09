974760eb8d884c00c60d96f0665cb06cdb01b9b2
lin.yen[顏琳] <lin.yen@104.com.tw>
2019-01-09 14:52:45 +0800

    modified

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ let box = document.querySelector('.box');
 let icon = document.querySelector('.icon');
 let header = document.querySelector('.header');
 let main = document.querySelector('.main');
+
+let forScroll = document.querySelector('.forScroll');
+
 icon.addEventListener('click',function(){
   box.classList.toggle('show');
   header.classList.toggle('m_header');
 
   main.classList.toggle('main_fix');
+
+  forScroll.classList.toggle('open');
 });
